0f5f17fce27c0340eb7be555a3e77cf42d62728a
Rachael-debug <rakinfoyewa@gmail.com>
2024-12-30 14:51:53 +0100

    Gave transition effects to the planets

--- jsFolder/destination.js
+++ jsFolder/destination.js
@@ -11,39 +11,65 @@ let mobileSlider = document.querySelector("#mobileSlider");
 
 
 moon.addEventListener("click", () => {
-    planetImage.src = "/cssFolder/assets/destination/image-moon.png";
+    
     planetName.textContent = "MOON";
     planetInfo.textContent = "See our planet as you've never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you're there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.";
     planetDistance.textContent = "384,400 KM";
     planetTime.textContent = "3 DAYS";
     mobileSlider.scrollTop = 0;
+
+
+    planetImage.classList.add("hidden");
+
+    setTimeout(() => {
+        planetImage.src = "/cssFolder/assets/destination/image-moon.png";
+        planetImage.classList.remove("hidden");
+    }, 500);
 });
 
 mars.addEventListener("click", () => {
-    planetImage.src = "/cssFolder/assets/destination/image-mars.png";
     planetName.textContent = "MARS";
     planetInfo.textContent = "Don't forget to pack your hiking boots. You'll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It's two and a half times the size of Everest!";
     planetDistance.textContent = "225 MIL. KM";
     planetTime.textContent = "9 MONTHS";
     mobileSlider.scrollTop = 0;
+
+    planetImage.classList.add("hidden");
+
+    setTimeout(() => {
+        planetImage.src = "/cssFolder/assets/destination/image-mars.png";
+        planetImage.classList.remove("hidden");
+    }, 500);
 });
 
 europa.addEventListener("click", () => {
-    planetImage.src = "/cssFolder/assets/destination/image-europa.png";
     planetName.textContent = "EUROPA";
     planetInfo.textContent = "The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover's dream. With an icy surface, it's perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.";
     planetDistance.textContent = "628 MIL. KM";
     planetTime.textContent = "3 YEARS";
     mobileSlider.scrollTop = 0;
+
+    planetImage.classList.add("hidden");
+
+    setTimeout(() => {
+        planetImage.src = "/cssFolder/assets/destination/image-europa.png";
+        planetImage.classList.remove("hidden");
+    }, 500);
 });
 
 titan.addEventListener("click", () => {
-    planetImage.src = "/cssFolder/assets/destination/image-titan.png";
     planetName.textContent = "TITAN";
     planetInfo.textContent = "The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.";
     planetDistance.textContent = "1.6 BIL. KM";
     planetTime.textContent = "7 YEARS";
     mobileSlider.scrollTop = 0;
+
+    planetImage.classList.add("hidden");
+
+    setTimeout(() => {
+        planetImage.src = "/cssFolder/assets/destination/image-titan.png";
+        planetImage.classList.remove("hidden");
+    }, 500);
 });
 
 
